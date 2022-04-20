08df9a1162c456b12019c535f574ca4cfc8dcbcd
Ernesto Aquino <thekingernesto@hotmail.com>
2022-04-20 21:42:44 +0200

    project creation

---
+++ LeBaluchon/LeBaluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeBaluchon
+//
+//  Created by Ernesto Elias Aquino Cifuentes on 20/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
