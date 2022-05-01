a498507cc8d87112a08ce131ffd6b53575d083d3
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-01 23:24:51 +0200

    Creation of function to verify if the amount written for the user is valid

--- LeBaluchon/LeBaluchon/Model/CurrencyConverterService.swift
+++ LeBaluchon/LeBaluchon/Model/CurrencyConverterService.swift
@@ -7,175 +7,104 @@
 
 import Foundation
 
+class CurrencyConverterService: CurrencyConverterDelegate {
 
-class TestCurrencyConverterService {
+    weak var viewDelegate: CurrencyConverterDelegate?
+    var currency: Currency = .USD
+    var moneyToConvert = ""
+    private var task: URLSessionDataTask?
+
+    private static let exchangeRateURL = URL(string: "http://data.fixer.io/api/latest?access_key=e9ef236194e10da371830069d966cc91&base=EUR&symbols=USD,MXN,JPY,GBP")!
     enum Currency: String {
-        case USD = "USD"
-        case MXN = "MXN"
-        case JPY = "JPY"
-        case GBP = "GBP"
+        case USD
+        case MXN
+        case JPY
+        case GBP
     }
 
-   
-    
-    private func getExchangeRate(completion: @escaping (ExchangeRate?, Error?) -> ()) {
-        let exchangeRateURL = URL(string: "http://data.fixer.io/api/latest?access_key=e9ef236194e10da371830069d966cc91&base=EUR&symbols=USD,MXN,JPY,GBP")!
-        
-        var request = URLRequest(url: exchangeRateURL)
+     private  func getExchangeRate(completion: @escaping (ExchangeRate?, Error?) -> Void) {
+         var request = URLRequest(url: CurrencyConverterService.exchangeRateURL)
         request.httpMethod = "GET"
-        
+
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { data, response, error in
-//            guard error == nil,
-//                  let data = data,
-//                    let response = response as? H else { return }
-            guard let data = data, let response = response as? HTTPURLResponse else { return }
-            guard response.statusCode != 200 else { return }
+        task?.cancel()
+        task = session.dataTask(with: request) { data, response, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("1. Error: \(error.localizedDescription)")
+                    return
+                }
+                guard let data = data, let response = response as? HTTPURLResponse else { return }
+                guard response.statusCode == 200 else { return }
 
-            let decoderExchange = JSONDecoder()
-            decoderExchange.keyDecodingStrategy = .convertFromSnakeCase
-            decoderExchange.dateDecodingStrategy = .secondsSince1970
-            
-            if let exchangeRate = try? decoderExchange.decode(ExchangeRate.self, from: data){
-                completion(exchangeRate, error)
-                
-                print("\(exchangeRate.rates)")
-                print (exchangeRate.base)
-                print (exchangeRate.timestamp)
-                
-                
-
-            }
-        }
-        
-        task.resume()
-
-    }
-    
-    
-    func test(){
-        getExchangeRate { exchangeRates, error in
-            
-        }
-    }
-    
-    
-    private func decoderData (data: Data) -> ExchangeRate? {
-        let decoderExchange = JSONDecoder()
-        decoderExchange.keyDecodingStrategy = .convertFromSnakeCase
-        decoderExchange.dateDecodingStrategy = .secondsSince1970
-        
-        if let exchangeRate = try? decoderExchange.decode(ExchangeRate.self, from: data){
-            print("\(exchangeRate.rates)")
-            return exchangeRate
-        }
-        return nil
-    }
-    
-}
-
-
-
-
-
-struct ExchangeRate : Decodable {
-    let success: Bool
-    let timestamp: Date
-    let base: String
-    let rates: [String : Float]
-}
-
-
-
-
-
-
-
-class CurrencyConverterService {
-    
-    private static let currencyConverterUrl =
-        URL(string: "http://data.fixer.io/api/latest?access_key=e9ef236194e10da371830069d966cc91&base=EUR&symbols=USD,MXN,JPY,GBP")!
-    
-    static func getExchangeRate(){
-        var request = URLRequest(url: currencyConverterUrl)
-        request.httpMethod = "GET"
-        
-//        let body = "access_key=e9ef236194e10da371830069d966cc91&base=EUR&symbols=USD,MXN,JPY,GBP"
-//        request.httpBody = body.data(using: .utf8)
-        
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: request) { data, response, error in
-            
-            if let error = error {
-                print("1. Error: \(error.localizedDescription)")
-                return
-            }
-            
-            if let data = data, let response = response as? HTTPURLResponse {
-                let statusCode = response.statusCode
-                if statusCode == 200 {
-                    do {
-                        let decoder = JSONDecoder()
-                        decoder.dateDecodingStrategy = .secondsSince1970
-                        let exchange = try decoder.decode(ExchangeRate.self, from: data)
-                        print (exchange.base)
-                        print (exchange.timestamp)
-                        if exchange.rates.count > 1 {
-                            print (exchange.rates["USD"]!)
-                        }
-                        
-                    } catch let error {
-                        print("2. Error: \(error.localizedDescription)")
-                    }
+                let decoderExchange = JSONDecoder()
+                decoderExchange.keyDecodingStrategy = .convertFromSnakeCase
+                decoderExchange.dateDecodingStrategy = .secondsSince1970
+                if let exchangeRates = try? decoderExchange.decode(ExchangeRate.self, from: data) {
+                    print("\(exchangeRates.rates)")
+                    print(exchangeRates.base)
+                    print(exchangeRates.timestamp)
+                    completion(exchangeRates, error)
+                } else {
+                    completion(nil, nil)
                 }
             }
         }
-//            if let dataResponse = data , error == nil{
-//                if let responseResponse = response as? HTTPURLResponse, responseResponse.statusCode == 200 {
-//                    print("Hasta aqui todo bien")
-//                    if  let exchangeRate =  try? JSONDecoder().decode(ExchangeRate.self, from: dataResponse){
-//                        print("funciono")
-//                    }
-//
-//                }
-//            }
-            
-//            if let data = data, error == nil {
-//                if let response = response as? HTTPURLResponse, response.statusCode == 200 {
-//                    let decoder = JSONDecoder()
-//                    decoder.keyDecodingStrategy = .convertFromSnakeCase
-//                    decoder.dateDecodingStrategy = .secondsSince1970
-//
-//
-//
-//
-//                    let exchangeRate = try?  decoder.decode(ExchangeRate.self, from: data)
-//                    print(exchangeRate?.base ?? "No funcionp")
-//
-//
-//
-//                    guard let resultado = exchangeRate?.success else {return}
-//                    print(resultado)
-//
-//                    if let responseJSON = try? JSONDecoder().decode([String:String].self, from: data){
-//                        let dollar = responseJSON["USD"],
-//                        let mexicanPeso = responseJSON["MXN"],
-//                        let yen =  responseJSON["JPY"],
-//                        let poundSterling = responseJSON["GBP"]{
-//
-//                        print ("iam here")
-//                    }
-//                }
-//            }
-            
-//            print(data ?? "No funciono")
-//            print(response ?? "No funciono response")
-//            print(error ?? "No funciono error")
-            
-       
-        task.resume()
+        task?.resume()
     }
- 
-    
-    
+
+    func doConversion(value: String?) {
+        guard stringWithEurosIsValid(value) else {
+        warningMessage("Please enter a valid amount (greater than 0 and less than 1 000 000).")
+        return }
+        getExchangeRate { exchangeRate, error in
+            guard error == nil,
+                  let exchangeRate = exchangeRate else {
+                self.warningMessage("We have un little problem, please check your internet connection.")
+                return
+            }
+            let resulat = self.calculateConversion(euros: value, exchangeData: exchangeRate)
+            self.refreshTextViewWithValue("\(resulat)")
+        }
+    }
+
+    func calculateConversion(euros: String?, exchangeData: ExchangeRate) -> String {
+        var conversionResult = -0.0
+
+        guard let euros = euros,
+              let eurosHowDouble = Double(euros),
+              !exchangeData.rates.isEmpty,
+              exchangeData.rates[currency.rawValue] != nil
+        else {
+            return String(conversionResult)
+        }
+        conversionResult = eurosHowDouble * exchangeData.rates[currency.rawValue]!
+        return String(conversionResult)
+    }
+
+    func stringWithEurosIsValid(_ value: String?) -> Bool {
+        guard let value = value,
+              let valueHowDouble = Double(value),
+              valueHowDouble > 0,
+              valueHowDouble < 1000000 else {return false}
+        return true
+    }
+
+    func warningMessage(_ message: String) {
+        guard let viewDelegate = viewDelegate else { return }
+        viewDelegate.warningMessage(message)
+    }
+
+    func refreshTextViewWithValue(_ value: String) {
+        guard let viewDelegate = viewDelegate else { return }
+        viewDelegate.refreshTextViewWithValue(value)
+    }
+
+}
+
+struct ExchangeRate: Decodable {
+    let success: Bool
+    let timestamp: Date
+    let base: String
+    let rates: [String: Double]
 }
