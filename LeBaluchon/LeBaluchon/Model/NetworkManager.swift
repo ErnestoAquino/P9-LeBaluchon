14824c668ea58b5bf0fe27ac396c0d39159d93a5
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-09 21:52:11 +0200

    Creation network manager  class

--- LeBaluchon/LeBaluchon/Model/NetworkManager.swift
+++ LeBaluchon/LeBaluchon/Model/NetworkManager.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+class NetworkManager<T: Decodable> {
+
+    private var task: URLSessionDataTask?
+    private let apiKey = Bundle.main.object(forInfoDictionaryKey: "TRANSLATOR_API_KEY") as? String
+
+    func getInformation(request: URLRequest?, completionHandler: @escaping (T?, Error?) -> Void) {
+        guard let request = request else { return }
+        let session = URLSession(configuration: .default)
+        task?.cancel()
+        task = session.dataTask(with: request, completionHandler: { data, response, error in
+            DispatchQueue.main.async {
+                guard error == nil,
+                      let data = data,
+                      let response = response as? HTTPURLResponse,
+                      response.statusCode == 200 else { return }
+                print(String(data: data, encoding: .utf8)!)
+                let decoderData = JSONDecoder()
+                decoderData.keyDecodingStrategy = .useDefaultKeys
+                guard let translation = try? decoderData.decode(T?.self, from: data) else { return }
+                completionHandler(translation, error)
+            }
+        })
+        task?.resume()
+    }
+
+    private func createURL() -> String {
+        guard let key = apiKey else { return ""}
+        let urlWithKey =
+        "http://translation.googleapis.com/language/translate/v2?key=\(key)&q=Hello my friend&source=en&target=fr&format=text"
+
+        return urlWithKey
+    }
+}
