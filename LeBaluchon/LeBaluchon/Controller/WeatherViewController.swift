e9d828bed626ced365413dda7eb38c6ccb426c91
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-09 23:23:55 +0200

    Delete func updateWeatherInformation

--- LeBaluchon/LeBaluchon/Controller/WeatherViewController.swift
+++ LeBaluchon/LeBaluchon/Controller/WeatherViewController.swift
@@ -16,7 +16,7 @@ class WeatherViewController: UIViewController {
         activityindicator.isHidden = true
         updateButton.round()
         weather.viewDelegate = self
-        weather.updateWeatherInformation()
+//        weather.updateWeatherInformation()
     }
 
     @IBOutlet weak var newYorkTextField: UITextView!
@@ -25,7 +25,7 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var activityindicator: UIActivityIndicatorView!
 
     @IBAction func tappedUdateButton() {
-        weather.updateWeatherInformation()
+        weather.test()
     }
 
 }
