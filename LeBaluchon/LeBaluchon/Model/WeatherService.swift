ed4342ef43bf93c2e2da9133e6004e4d884e9e80
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-14 00:13:54 +0200

    Comments on the functions. Modification of the update weather informaion

--- LeBaluchon/LeBaluchon/Model/WeatherService.swift
+++ LeBaluchon/LeBaluchon/Model/WeatherService.swift
@@ -16,11 +16,8 @@ class WeatherService {
     private var networkManager = NetworkManager<WeatherData>()
     private let message = "Sorry, we have a little problem please check your internet connection."
 
-    func updateWeatherInformation() {
-        updateWatherInformationForBreval()
-        updateWeatherInformationForNewYork()
-    }
-
+    // This function retrives the information to be displayed to the user from a Weather Data
+    // structure and stores it ini a string.
     private func createTextForUpadateInformation(_ weather: WeatherData) -> String {
         var text = "Sorry, we have a little problem."
         guard let description = weather.weather?[0].description,
@@ -37,51 +34,39 @@ class WeatherService {
         """
         return text
     }
-
-    private func createUrlFor(_ city: City) -> String {
-        guard let key = weatherApiKey else { return "" }
-        let urlWithKey =
-        "\(urlBase)?lat=\(city.latitude)&lon=\(city.longitude)&appid=\(key)&units=metric"
-
-        return urlWithKey
-    }
-
+    // This function create a URL Request for URL Session.
+    // Receives as parameter the city from which you want to
+    // obtain the weather information.
     private func createRequestFor(_ city: City) -> URLRequest? {
-        guard let urlWeather = URL(string: createUrlFor(city)) else {return nil}
+        guard let key = weatherApiKey else {return nil}
+        let urlWithKey = "\(urlBase)?lat=\(city.latitude)&lon=\(city.longitude)&appid=\(key)&units=metric"
+        guard let urlWeather = URL(string: urlWithKey) else {return nil}
         var request = URLRequest(url: urlWeather)
         request.httpMethod = "GET"
 
         return request
     }
-
-    private func updateWatherInformationForBreval() {
-        guard let requestForBreval = createRequestFor(breval) else {
-            warningMessage(message)
-            return
-        }
-        networkManager.getInformation(request: requestForBreval) { weatherData, error in
+    // This function retrives wether information for two cities, Breval and NewYork.
+    // It creates a request for each one. Using the method of the network manager class
+    // it retrives the information.
+    func updateWeatherInformation() {
+        guard let requestForBreval = createRequestFor(breval),
+              let requestForNewYork = createRequestFor(newYork) else {return}
+        networkManager.getInformation(request: requestForBreval) { weatherBreval, error in
             guard error == nil,
-                  let weatherData = weatherData else {
+                  let weatherBreval = weatherBreval else {
                 self.warningMessage(self.message)
                 return
             }
-            self.refreshBrevalTextFieldWith(self.createTextForUpadateInformation(weatherData))
-        }
-    }
-
-    private func updateWeatherInformationForNewYork() {
-        guard let requestForNewYork = createRequestFor(newYork) else {
-            warningMessage(message)
-            return
-        }
-        let networkManagerTest = NetworkManager<WeatherData>()
-        networkManagerTest.getInformation(request: requestForNewYork) { weatherData, error in
-            guard error == nil,
-                  let weatherDataForNewYork = weatherData else {
-                self.warningMessage(self.message)
-                return
+            self.refreshBrevalTextFieldWith(self.createTextForUpadateInformation(weatherBreval))
+            self.networkManager.getInformation(request: requestForNewYork) { weatherNewYork, error in
+                guard error == nil,
+                      let weatherNewYork = weatherNewYork else {
+                    self.warningMessage(self.message)
+                    return
+                }
+                self.refreshNewYorkTextFieldWith(self.createTextForUpadateInformation(weatherNewYork))
             }
-            self.refreshNewYorkTextFieldWith(self.createTextForUpadateInformation(weatherDataForNewYork))
         }
     }
 }
