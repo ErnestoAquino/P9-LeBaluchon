09769a7544b92530739a90c3f6472f51ff88137f
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-23 16:32:50 +0200

    Creating test for currency converter class

--- LeBaluchon/LeBaluchonTests/CurrencyConverterServiceTestCase.swift
+++ LeBaluchon/LeBaluchonTests/CurrencyConverterServiceTestCase.swift
@@ -6,30 +6,31 @@
 //
 
 import XCTest
+@testable import LeBaluchon
 
 class CurrencyConverterServiceTestCase: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testGivenCurrencyIsUsd_WhenIselectMnx_ThenCurencyIsMnx() {
+        let currencyConverterService =  CurrencyConverterService()
+        // Given
+        currencyConverterService.currency = .USD
+        // When
+        currencyConverterService.currency = .MXN
+        // Then
+        XCTAssertTrue(currencyConverterService.currency == .MXN)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testGivenAnInvaidAmount_WhenLaunchDoConversion_ThenWarningMessageIsCalled() {
+        let mockDelegate = CurrencyConverterMockDelegate()
+        let currencyConverterService =  CurrencyConverterService()
+        currencyConverterService.viewDelegate = mockDelegate
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        // Given
+        let wrongAmount = "I am a wrong amout"
+        // When
+        currencyConverterService.doConversion(eurosToBeConverted: wrongAmount)
+        // Then
+        XCTAssertTrue(mockDelegate.warningMessageIsCalled)
     }
 
 }
