7c3149757a78ac467c04097b92d4032628dc7a20
Ernesto Elias Aquino Cifuentes <ernesto@macbook-pro-de-ernesto.home>
2022-05-27 00:38:06 +0200

    writing tests for the weather information class

--- LeBaluchon/LeBaluchonTests/WeatherServiceTestCase.swift
+++ LeBaluchon/LeBaluchonTests/WeatherServiceTestCase.swift
@@ -10,35 +10,93 @@ import XCTest
 
 class WeatherServiceTestCase: XCTestCase {
 
-    func testGivenCorrectResponse_WhenUpdateWeatherInformation_ThenSuccessShoulBeTrue() {
-        let expectations = expectation(description: "Wait for queue change.")
-        // Given
-        let delegate = WeatherMockDelegate()
-        let session = URLSessionFake(data: FakeResponse.WeatherCorrectData, response: FakeResponse.responseOK, error: nil)
-        let service = WeatherService(session)
-        service.viewDelegate = delegate
-        // When
-        service.updateWeatherInformation { succes in
-            // Then
-            XCTAssertTrue(succes)
-            expectations.fulfill()
-        }
-        wait(for: [expectations], timeout: 0.01)
+    var mockDelegate: WeatherMockDelegate?
+
+    override func setUp() {
+        mockDelegate = WeatherMockDelegate()
     }
 
-    func testGivenErrorIntResponse_WhenUpdateWeatherInformation_ThenSuccessShoulBeFalse() {
-        let expectations = expectation(description: "Wait for queue change.")
+    func testGivenErrorInResponse_WhenUpdateWeatherInformation_ThenWarningMessageIsCalled() async {
+        guard let mockDelegate = mockDelegate else { return }
+        let exp = expectation(description: "Wait to the function to terminate")
         // Given
-        let delegate = WeatherMockDelegate()
-        let session = URLSessionFake(data: FakeResponse.WeatherCorrectData, response: FakeResponse.responseOK, error: FakeResponse.anError)
-        let service = WeatherService(session)
-        service.viewDelegate = delegate
+        let session = URLSessionFake(data: FakeResponse.translationCorrectData, response: FakeResponse.responseOK, error: FakeResponse.anError)
+        let weatherService = WeatherService(session)
+        weatherService.viewDelegate = mockDelegate
+
         // When
-        service.updateWeatherInformation { succes in
-            // Then
-            XCTAssertFalse(succes)
-            expectations.fulfill()
-        }
-        wait(for: [expectations], timeout: 0.01)
+        weatherService.updateWeatherInformation()
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        // Then
+        XCTAssert(mockDelegate.warningMessageIsCalled)
+    }
+
+    func testGivenWrongDataInResponse_WhenUpdateWeatherInformation_ThenWarningMessageIsCalled() async {
+        guard let mockDelegate = mockDelegate else { return }
+        let exp = expectation(description: "Wait to the function to terminate")
+        // Given
+        let session = URLSessionFake(data: FakeResponse.incorretData, response: FakeResponse.responseOK, error: nil)
+        let weatherService = WeatherService(session)
+        weatherService.viewDelegate = mockDelegate
+        // When
+        weatherService.updateWeatherInformation()
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        //
+        XCTAssert(mockDelegate.warningMessageIsCalled)
+    }
+
+    func testGivenCorrectResponse_WhenUpdateWeatherInformation_ThenRefreshTextsFieldShouldBeCalled() async {
+        guard let mockDelegate = mockDelegate else { return }
+        let exp = expectation(description: "Wait to the function to terminate")
+        // Given
+        let session = URLSessionFake(data: FakeResponse.WeatherCorrectData, response: FakeResponse.responseOK, error: nil)
+        let weatherService = WeatherService(session)
+        weatherService.viewDelegate = mockDelegate
+        // When
+        weatherService.updateWeatherInformation()
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        // Then
+        XCTAssert(mockDelegate.refreshNewYorkTextFieldWithIsCalled)
+    }
+
+    func testGivenCorrectResponse_WhenUpdateWeatherInformation_ThenlGetTheExpectedInformation() async {
+        guard let mockDelegate = mockDelegate else { return }
+        let exp = expectation(description: "Wait to the function to terminate")
+        // Given
+        let session = URLSessionFake(data: FakeResponse.WeatherCorrectData, response: FakeResponse.responseOK, error: nil)
+        let weatherServive = WeatherService(session)
+        weatherServive.viewDelegate = mockDelegate
+        let informationExpected =
+        """
+        SCATTERED CLOUDS
+        16.08 °C min - 26.31 °C max
+        7.72 Km/h
+        74.0 % Humidity
+        """
+        // When
+        weatherServive.updateWeatherInformation()
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        // Then
+        XCTAssertEqual(informationExpected, weatherServive.brevalWeatherInformation)
+        XCTAssertEqual(informationExpected, weatherServive.newyorkWeatherInformation)
+    }
+
+    func testGivenWrongStatusCodeInResponse_WhenUpdateWeatherInformation_ThenWarningMessageIsCalled() async {
+        guard let mockDelegate = mockDelegate else { return }
+        let exp = expectation(description: "Wait to the function to terminate")
+        // Given
+        let session = URLSessionFake(data: FakeResponse.WeatherCorrectData, response: FakeResponse.responseFail, error: nil)
+        let serviceWeather = WeatherService(session)
+        serviceWeather.viewDelegate = mockDelegate
+        // When
+        serviceWeather.updateWeatherInformation()
+        exp.fulfill()
+        await waitForExpectations(timeout: 1)
+        // Then
+        XCTAssert(mockDelegate.warningMessageIsCalled)
     }
 }
