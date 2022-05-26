727193142a58901892363093885106c4a2f74d4c
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-26 16:54:31 +0200

    The doTranslation function has been split. same as the create request function

--- LeBaluchon/LeBaluchon/Model/TranslatorService.swift
+++ LeBaluchon/LeBaluchon/Model/TranslatorService.swift
@@ -12,53 +12,67 @@ final class TranslateService {
     private let apiKey = Bundle.main.object(forInfoDictionaryKey: "TRANSLATOR_API_KEY") as? String
     private let urlBase = "https://translation.googleapis.com/language/translate/v2"
     private let session: URLSessionProtocol
-    private (set) var translatedTex: String?
+    private (set) var translatedTex = ""
 
     init(_ session: URLSessionProtocol) {
         self.session = session
     }
 
-    // This function does the translation, it uses the getInformation method  of the
-    // network manager class to request the translation, and retrives the translated text.
-    public func doTranslation(textForTranslation: String?, completion: @escaping (Bool) -> Void) {
-        let networkManager =  NetworkManager<TranslationResponse>(networkManagerSession: session)
-        guard let textForTranslation = textForTranslation else {
-            warningMessage("Sorry, but your text is not valid")
-            completion(false)
-            return
-        }
-        guard let resquest = createRequest(textForTranslation) else {
-            warningMessage("Sorry, we have a little problem.")
-            return
-        }
-        toogleActivityIndicator(shown: true)
-        networkManager.getInformation(request: resquest) { translationResponse, error in
-                self.toogleActivityIndicator(shown: false)
-                guard error == nil else {
-                    self.warningMessage("Sorry, we have a little problem.")
-                    completion(false)
-                    return
-                }
-                 guard let translatedText = translationResponse?.data?.translations?[0].translatedText else {
-                     self.warningMessage("Sorry, we have a little problem.")
-                     completion(false)
-                     return
-                 }
-            self.refreshEnglishTextFieldWith(translatedText)
-            completion(true)
+    public func doTranslation(textToTranslate: String?) {
+        obtainTranslation(textToTranslate) { textTranslated, succes in
+            guard succes == true,
+                  let textTranslated = textTranslated else {
+                self.warningMessage("Sorry, we have a little problem")
+                return
+            }
+            self.translatedTex = textTranslated
+            self.refreshEnglishTextFieldWith(self.translatedTex)
         }
     }
 
-    // This function create a URL Request for URL Session.
-    // Receives as a parameter the text to be translated and adds it to the request body.
-     private func createRequest(_ textForTranslation: String) -> URLRequest? {
-        guard let urlTranslation = URL(string: urlBase),
-              let key = apiKey else {return nil}
-        var request = URLRequest(url: urlTranslation)
+    private func obtainTranslation(_ textToTranslate: String?, completion: @escaping (String?, Bool?) -> Void) {
+        let networkManager = NetworkManager<TranslationResponse>(networkManagerSession: session)
+        guard let textToTranslate = textToTranslate else {
+            warningMessage("Sorry, but your text is not valid")
+            completion(nil, false)
+            return
+        }
+        let request = creteRequest(textToTranslate)
+        toogleActivityIndicator(shown: true)
+        networkManager.getInformation(request: request) { translationResponse, error in
+            self.toogleActivityIndicator(shown: false)
+            guard error == nil,
+                  let translatedText = translationResponse?.data?.translations?[0].translatedText else {
+                self.warningMessage("Sorry, we have a little problem")
+                completion(nil, false)
+                return
+            }
+            completion(translatedText, true)
+        }
+    }
+
+    private func creteRequest(_ textForTranslation: String) -> URLRequest {
+        let url =  createURL()
+        let key = createKey()
+        var request = URLRequest(url: url)
         request.httpMethod = "POST"
         let body = "key=\(key)&q=\(textForTranslation)&source=fr&target=en&format=text"
         request.httpBody = body.data(using: .utf8)
 
         return request
+    }
+
+    private func createURL() -> URL {
+        guard let urlTranslation = URL(string: urlBase) else {
+            return  URL(string: " ")!
+        }
+        return urlTranslation
+    }
+
+    private func createKey() -> String {
+        guard let key = apiKey else {
+            return " "
+        }
+        return key
     }
 }
