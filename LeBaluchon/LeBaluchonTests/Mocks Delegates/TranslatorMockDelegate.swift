d015dd179d28e4d556b8fadbe173147f5ebbb9f8
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-27 14:56:21 +0200

    Creation translator mock delegate

--- LeBaluchon/LeBaluchonTests/Mocks Delegates/TranslatorMockDelegate.swift
+++ LeBaluchon/LeBaluchonTests/Mocks Delegates/TranslatorMockDelegate.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+@testable import LeBaluchon
+
+// Mock delegate created to test the TranslationService class.
+class TranslatorMockDelegate: TranslatorDelegate {
+    var warningMessageIsCalled = false
+    var refreshEnglishTextFieldWithIsCalled = false
+    var toogleActivityIndicatorIsCalled = false
+
+    func warningMessage(_ message: String) {
+        warningMessageIsCalled = true
+    }
+
+    func refreshEnglishTextFieldWith(_ translatedTex: String) {
+        refreshEnglishTextFieldWithIsCalled = true
+    }
+
+    func toogleActivityIndicator(shown: Bool) {
+        toogleActivityIndicatorIsCalled = true
+    }
+}
