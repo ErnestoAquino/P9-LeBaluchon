eca89ac2ec23ce2d26473004faf6d2cc68a67bd9
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-27 13:58:53 +0200

    deletion of the mocks delegates file and creation of a folder for each file

---
+++ LeBaluchon/LeBaluchonTests/Mocks Delegates/CurrencyConverterMockDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyConverterMockDelegate.swift
+//  LeBaluchonTests
+//
+//  Created by Ernesto Elias on 27/05/2022.
+//
+
+import Foundation
---
+++ LeBaluchon/LeBaluchonTests/Mocks Delegates/WeatherMockDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherMockDelegate.swift
+//  LeBaluchonTests
+//
+//  Created by Ernesto Elias on 27/05/2022.
+//
+
+import Foundation
