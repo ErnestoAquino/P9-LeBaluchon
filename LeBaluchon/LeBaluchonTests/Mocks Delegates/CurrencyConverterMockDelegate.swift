7241d4d847847580f7f07b263e00444ae8739e42
Ernesto Aquino <thekingernesto@hotmail.com>
2022-05-27 14:55:33 +0200

    Creation Currency converter mock delegate

--- LeBaluchon/LeBaluchonTests/Mocks Delegates/CurrencyConverterMockDelegate.swift
+++ LeBaluchon/LeBaluchonTests/Mocks Delegates/CurrencyConverterMockDelegate.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+@testable import LeBaluchon
+
+// Mock delegate  to test the CurrentConverterService class.
+class CurrencyConverterMockDelegate: CurrencyConverterDelegate {
+
+    var warningMessageIsCalled = false
+    var refreshTextViewWithValueIsCalled = false
+    var toogledActivityIndicatorIsCalled = false
+
+    func warningMessage(_ message: String) {
+        warningMessageIsCalled = true
+    }
+
+    func refreshTextViewWithValue(_ value: String) {
+        refreshTextViewWithValueIsCalled = true
+    }
+
+    func toogleActivityIndicator(shown: Bool) {
+        toogledActivityIndicatorIsCalled = true
+    }
+}
